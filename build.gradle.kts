94d06b9ad6803d69cac154cf437b92b9a2b92922
Phodal Huang <h@phodal.com>
2020-08-15 15:48:02 +0800

    build: disable kotlin lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     kotlin("jvm") version BuildPluginsVersion.KOTLIN apply false
-    id("io.gitlab.arturbosch.detekt") version BuildPluginsVersion.DETEKT
-    id("org.jlleitschuh.gradle.ktlint") version BuildPluginsVersion.KTLINT
+//    id("io.gitlab.arturbosch.detekt") version BuildPluginsVersion.DETEKT
+//    id("org.jlleitschuh.gradle.ktlint") version BuildPluginsVersion.KTLINT
     id("com.github.ben-manes.versions") version BuildPluginsVersion.VERSIONS_PLUGIN
 }
 
@@ -17,33 +17,33 @@ allprojects {
 
 subprojects {
     apply {
-        plugin("io.gitlab.arturbosch.detekt")
-        plugin("org.jlleitschuh.gradle.ktlint")
+//        plugin("io.gitlab.arturbosch.detekt")
+//        plugin("org.jlleitschuh.gradle.ktlint")
     }
+//
+//    ktlint {
+//        debug.set(false)
+//        version.set(Versions.KTLINT)
+//        verbose.set(true)
+//        android.set(false)
+//        outputToConsole.set(true)
+//        ignoreFailures.set(false)
+//        enableExperimentalRules.set(true)
+//        filter {
+//            exclude("**/generated/**")
+//            include("**/kotlin/**")
+//        }
+//    }
 
-    ktlint {
-        debug.set(false)
-        version.set(Versions.KTLINT)
-        verbose.set(true)
-        android.set(false)
-        outputToConsole.set(true)
-        ignoreFailures.set(false)
-        enableExperimentalRules.set(true)
-        filter {
-            exclude("**/generated/**")
-            include("**/kotlin/**")
-        }
-    }
-
-    detekt {
-        config = rootProject.files("config/detekt/detekt.yml")
-        reports {
-            html {
-                enabled = true
-                destination = file("build/reports/detekt.html")
-            }
-        }
-    }
+//    detekt {
+//        config = rootProject.files("config/detekt/detekt.yml")
+//        reports {
+//            html {
+//                enabled = true
+//                destination = file("build/reports/detekt.html")
+//            }
+//        }
+//    }
 }
 
 tasks.withType<DependencyUpdatesTask> {
