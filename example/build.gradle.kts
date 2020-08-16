7f6736b31ac5d74a16d11ed2d5ec618c7be33b76
Phodal Huang <h@phodal.com>
2020-08-16 20:35:19 +0800

    feat: add joda time for testing deps

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 dependencies {
 //    implementation(gradleApi())
     implementation("net.sf.proguard:proguard-gradle:5.2.1")
+    implementation("joda-time:joda-time:2.10.6")
 //    androidTestImplementation("androidx.test.ext:junit:1.1.1")
 //    phodalImpl()
 }
--- example/src/main/java/HelloWorld.java
+++ example/src/main/java/HelloWorld.java
@@ -1,5 +1,33 @@
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+
+import java.util.Locale;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello, world!");
+    }
+
+    public boolean isAfterPayDay(DateTime datetime) {
+        if (datetime.getMonthOfYear() == 2) {   // February is month 2!!
+            return datetime.getDayOfMonth() > 26;
+        }
+        return datetime.getDayOfMonth() > 28;
+    }
+
+    public Days daysToNewYear(LocalDate fromDate) {
+        LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+        return Days.daysBetween(fromDate, newYear);
+    }
+
+    public boolean isRentalOverdue(DateTime datetimeRented) {
+        Period rentalPeriod = new Period().withDays(2).withHours(12);
+        return datetimeRented.plus(rentalPeriod).isBeforeNow();
+    }
+
+    public String getBirthMonthText(LocalDate dateOfBirth) {
+        return dateOfBirth.monthOfYear().getAsText(Locale.ENGLISH);
     }
 }
