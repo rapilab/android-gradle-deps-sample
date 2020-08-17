fee7d7461d271bd609ceec09f5b9bcd35c707019
Phodal Huang <h@phodal.com>
2020-08-17 21:37:56 +0800

    refactor: extract codes

--- plugin-build/plugin/src/main/java/com/phodal/gradoid/PhodalCompile.kt
+++ plugin-build/plugin/src/main/java/com/phodal/gradoid/PhodalCompile.kt
@@ -1,9 +1,9 @@
 package com.phodal.gradoid
 
 import com.google.common.collect.ImmutableList
-import com.google.common.collect.Sets
 import com.phodal.gradoid.internal.api.DefaultAndroidSourceSet
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.artifacts.ResolutionStrategy
 import org.gradle.api.file.ConfigurableFileTree
 import org.gradle.api.file.FileTree
@@ -15,29 +15,17 @@ open class PhodalCompile() : JavaCompile() {
     var compileClasspaths: MutableSet<Configuration> = mutableSetOf()
 
     fun configure() {
-        val javaSourceSet = DefaultAndroidSourceSet("main", project)
-        val sourceSets = ImmutableList.builder<ConfigurableFileTree>()
-        sourceSets.addAll(javaSourceSet.javaSource.getSourceDirectoryTrees()!!)
-        val source = sourceSets.build()
-
-        val javaSource = mutableListOf<FileTree>()
-        javaSource.addAll(source)
+        val javaSource = getJavaSources()
 
         val configurations = project.configurations
-        val config: Configuration = configurations.getByName("implementation")
-        val compileClasspath: Configuration = configurations.getByName("phodalImplementation")
 
+        val config: Configuration = configurations.getByName("implementation")
         compileClasspaths.add(config)
+
+        val compileClasspath: Configuration = configurations.getByName("phodalImplementation")
         compileClasspaths.add(compileClasspath)
 
-        val finalPath = configurations.maybeCreate("finalCompileClasspath")
-        finalPath.isVisible = false
-        finalPath.description = "Resolved configuration for compilation for variant"
-        finalPath.setExtendsFrom(compileClasspaths)
-        finalPath.isCanBeConsumed = false
-        finalPath.resolutionStrategy.sortArtifacts(ResolutionStrategy.SortOrder.CONSUMER_FIRST)
-
-        this.classpath = finalPath
+        this.classpath = classPathsConfig(configurations, compileClasspaths)
 
         this.source = this.project.files(javaSource.toList()).asFileTree
 
@@ -46,6 +34,30 @@ open class PhodalCompile() : JavaCompile() {
 
         this.options.isIncremental = true
 //        this.options.bootstrapClasspath => android.jar
+    }
+
+    private fun getJavaSources(): MutableList<FileTree> {
+        val javaSourceSet = DefaultAndroidSourceSet("main", project)
+        val sourceSets = ImmutableList.builder<ConfigurableFileTree>()
+        sourceSets.addAll(javaSourceSet.javaSource.getSourceDirectoryTrees()!!)
+        val source = sourceSets.build()
+
+        val javaSource = mutableListOf<FileTree>()
+        javaSource.addAll(source)
+        return javaSource
+    }
+
+    private fun classPathsConfig(
+        configurations: ConfigurationContainer,
+        mutableSet: MutableSet<Configuration>
+    ): Configuration {
+        val finalPath = configurations.maybeCreate("finalCompileClasspath")
+        finalPath.isVisible = false
+        finalPath.description = "Resolved configuration for compilation for variant"
+        finalPath.setExtendsFrom(mutableSet)
+        finalPath.isCanBeConsumed = false
+        finalPath.resolutionStrategy.sortArtifacts(ResolutionStrategy.SortOrder.CONSUMER_FIRST)
+        return finalPath
     }
 
     override fun compile(inputs: InputChanges) {
