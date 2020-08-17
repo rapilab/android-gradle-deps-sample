fba0af275d97d0e039aa1c1ddba1e627feb18991
Phodal Huang <h@phodal.com>
2020-08-17 21:06:38 +0800

    docs: remove unused code in examples deps

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
 //    java
     id("com.phodal.gradoid")
 }
-//
-//templateExampleConfig {
-//    message.set("Just trying this gradle plugin...")
-//}
 
 repositories {
     mavenCentral()
@@ -15,6 +11,4 @@ dependencies {
 //    implementation(gradleApi())
     implementation("net.sf.proguard:proguard-gradle:5.2.1")
     phodalImplementation("joda-time:joda-time:2.10.6")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-//    zzz("org.gradle:gradle-core:2.2")
 }
